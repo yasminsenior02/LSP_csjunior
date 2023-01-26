2f57100be73ba4e98a8434f4f276139b4ff43651
yasminsenior02 <Yasmin.senior@bison.howard.edu>
2023-01-25 21:59:52 -0500

    printing hello world, code to output to stdout

--- lspspring2023/src/org/howard/edu/hw1/HelloWorld.java
+++ lspspring2023/src/org/howard/edu/hw1/HelloWorld.java
@@ -3,7 +3,7 @@ package org.howard.edu.hw1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!!");
 
 	}
 
